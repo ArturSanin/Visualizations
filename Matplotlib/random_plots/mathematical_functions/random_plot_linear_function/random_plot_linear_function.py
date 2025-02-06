ff8c5de107d5a10ba674c027d0d9072f772786a9
Artur Sanin <artur.sanin@web.de>
2025-02-06 05:05:27 +0100

    Random plot linear function.

--- Matplotlib/random_plots/mathematical_functions/random_plot_linear_function/random_plot_linear_function.py
+++ Matplotlib/random_plots/mathematical_functions/random_plot_linear_function/random_plot_linear_function.py
@@ -1,106 +1,125 @@
-"""
-    Description: This program generates a random plot of a linear function
-    f(x) = a * x + b.
-"""
-
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 def random_linear_plot():
-    if (2 * np.random.randint(2) - 1) > 0:  # Random condition.
+    """
+        This program generates a random plot of a linear function f(x) = a * x + b.
+    """
 
+    # Defining the coefficient of the linear function through a random condition.
+    if (2 * np.random.randint(2) - 1) > 0:
         a = (2 * np.random.randint(2) - 1) * np.random.randint(10)
-        b = (2 * np.random.randint(2) - 1) * np.random.randint(10)
-
-        if a == 0:
-
-            x = np.linspace(- 5, 5, 100)
-            y = []
-
-            counter_1 = 0
-
-            while counter_1 < len(x):
-                y.append(b)
-                counter_1 += 1
-
-            fig, ax = plt.subplots()
-
-            ax.plot(x, y)  # Plots the x and y values.
-
-            plt.xlim(np.min(x), np.max(x))
-
-            plt.title('Plot Linear Function ' + 'y = ' + str(np.round(b, 5)))  # Plot title.
-            plt.xlabel('x')  # x-axis label.
-            plt.ylabel('y')  # y-axis label.
-
-            plt.show()
-        else:
-            x = np.linspace(- np.round(np.divide(b, a), 0), 5, 100)
-            y = a * x + b
-
-            fig, ax = plt.subplots()
-
-            ax.plot(x, y)  # Plots the x and y values.
-
-            plt.xlim(np.min(x), np.max(x))
-
-            if b > 0:
-                plt.title('Plot Linear Function ' + 'y = ' + str(a) + ' * x' + ' + ' + str(b))  # Plot title.
-            else:
-                plt.title(
-                    'Plot Linear Function ' + 'y = ' + str(a) + ' * x' + ' - ' + str(np.absolute(b)))  # Plot title.
-            plt.xlabel('x')  # x-axis label.
-            plt.ylabel('y')  # y-axis label.
-
-            plt.show()
     else:
-        a = (2 * np.random.randint(2) - 1) * np.random.randint(10) * np.random.random_sample()
-        b = (2 * np.random.randint(2) - 1) * np.random.randint(10) * np.random.random_sample()
+        a = (2 * np.random.randint(2) - 1) * np.round(np.random.randint(10) * np.random.random_sample(),
+                                                      np.random.randint(1, 6))
 
-        if a == 0:
+    if (2 * np.random.randint(2) - 1) > 0:
+        b = (2 * np.random.randint(2) - 1) * np.random.randint(10)
+    else:
+        b = (2 * np.random.randint(2) - 1) * np.round(np.random.randint(10) * np.random.random_sample(),
+                                                      np.random.randint(1, 6))
 
-            x = np.linspace(- 5, 5, 100)
-            y = []
+    # Depending on the values of the coefficients, a plot will be displayed.
+    if (a == 0) and (b == 0):
+        x = np.linspace(- 5, 5, 100)
+        y = []
 
-            counter_2 = 0
+        counter_1 = 0
+        while counter_1 < len(x):
+            y.append(0)
+            counter_1 += 1
 
-            while counter_2 < len(x):
-                y.append(b)
-                counter_2 += 1
+        fig, ax = plt.subplots()
 
-            fig, ax = plt.subplots()
+        ax.plot(x, y)  # Plots the x and y values.
 
-            ax.plot(x, y)  # Plots the x and y values.
+        plt.xlim(np.min(x), np.max(x))
 
-            plt.xlim(np.min(x), np.max(x))
+        plt.title('Plot Linear Function ' + 'y = ' + str(0))  # Plot title.
 
-            plt.title('Plot Linear Function ' + 'y = ' + str(np.round(b, 5)))  # Plot title.
-            plt.xlabel('x')  # x-axis label.
-            plt.ylabel('y')  # y-axis label.
+        # Axis labels.
+        plt.xlabel('x')
+        plt.ylabel('y')
 
-            plt.show()
+        plt.show()
+
+    elif a == 0:
+        x = np.linspace(- 5, 5, 100)
+        y = []
+
+        counter_2 = 0
+        while counter_2 < len(x):
+            y.append(b)
+            counter_2 += 1
+
+        fig, ax = plt.subplots()
+
+        ax.plot(x, y)  # Plots the x and y values.
+
+        plt.xlim(np.min(x), np.max(x))
+
+        plt.title('Plot Linear Function ' + 'y = ' + str(b))  # Plot title.
+
+        # Axis labels.
+        plt.xlabel('x')
+        plt.ylabel('y')
+
+        plt.show()
+
+    elif b == 0:
+        x = np.linspace(0, 5, 100)
+        y = a * x
+
+        fig, ax = plt.subplots()
+
+        ax.plot(x, y)  # Plots the x and y values.
+
+        plt.xlim(np.min(x), np.max(x))
+
+        # Displaying the plot title depending on the value of a.
+        if a == 1:
+            plt.title('Plot Linear Function ' + 'y = ' + 'x')
+        elif a == -1:
+            plt.title('Plot Linear Function ' + 'y = ' + '-x')
         else:
-            x = np.linspace(- np.round(np.divide(b, a), 0), 5, 100)
-            y = a * x + b
+            plt.title('Plot Linear Function ' + 'y = ' + str(a) + ' * x')
 
-            fig, ax = plt.subplots()
+        # Axis labels.
+        plt.xlabel('x')
+        plt.ylabel('y')
 
-            ax.plot(x, y)  # Plots the x and y values.
+        plt.show()
 
-            plt.xlim(np.min(x), np.max(x))
+    else:
+        x = np.linspace(- np.round(np.divide(b, a), 0), 5, 100)
+        y = a * x + b
 
-            if b > 0:
-                plt.title('Plot Linear Function ' + 'y = ' + str(np.round(a, 5)) + ' * x' + ' + '
-                          + str(np.round(b, 5)))  # Plot title.
-            else:
-                plt.title('Plot Linear Function ' + 'y = ' + str(np.round(a, 5)) + ' * x' + ' - '
-                          + str(np.absolute(np.round(b, 5))))  # Plot title.
+        fig, ax = plt.subplots()
 
-            plt.xlabel('x')  # x-axis label.
-            plt.ylabel('y')  # y-axis label.
+        ax.plot(x, y)  # Plots the x and y values.
 
-            plt.show()
+        plt.xlim(np.min(x), np.max(x))
+
+        # Displaying the plot title depending on the values of a and b.
+        if (a == 1) and (b > 0):
+            plt.title('Plot Linear Function ' + 'y = ' + 'x' + ' + ' + str(b))
+        elif (a == 1) and (b < 0):
+            plt.title('Plot Linear Function ' + 'y = ' + 'x' + ' - ' + str(np.absolute(b)))
+        elif (a == -1) and (b > 0):
+            plt.title('Plot Linear Function ' + 'y = ' + '-x' + ' + ' + str(b))
+        elif (a == -1) and (b < 0):
+            plt.title('Plot Linear Function ' + 'y = ' + '-x' + ' - ' + str(np.absolute(b)))
+        elif b > 0:
+            plt.title('Plot Linear Function ' + 'y = ' + str(a) + ' * x' + ' - ' + str(b))
+        else:
+            plt.title('Plot Linear Function ' + 'y = ' + str(a) + ' * x' + ' - ' + str(np.absolute(b)))
+
+        # Axis labels.
+        plt.xlabel('x')
+        plt.ylabel('y')
+
+        plt.show()
 
 
 random_linear_plot()
